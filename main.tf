253cf7741b2cf7d6f158fdf3dd5938b21c1e2d19
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:31:06 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -50,36 +50,36 @@ locals {
 }
 
 module "storage_movers" {
-  source         = "git::https://github.com/AeternaModules/azurerm_storage_mover.git?ref=v4.80.0"
+  source         = "git::https://github.com/AeternaModules/azurerm_storage_mover.git?ref=v4.81.0"
   storage_movers = local.storage_movers
 }
 
 module "storage_mover_agents" {
-  source               = "git::https://github.com/AeternaModules/azurerm_storage_mover_agent.git?ref=v4.80.0"
+  source               = "git::https://github.com/AeternaModules/azurerm_storage_mover_agent.git?ref=v4.81.0"
   storage_mover_agents = local.storage_mover_agents
   depends_on           = [module.storage_movers]
 }
 
 module "storage_mover_projects" {
-  source                 = "git::https://github.com/AeternaModules/azurerm_storage_mover_project.git?ref=v4.80.0"
+  source                 = "git::https://github.com/AeternaModules/azurerm_storage_mover_project.git?ref=v4.81.0"
   storage_mover_projects = local.storage_mover_projects
   depends_on             = [module.storage_movers]
 }
 
 module "storage_mover_source_endpoints" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_storage_mover_source_endpoint.git?ref=v4.80.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_storage_mover_source_endpoint.git?ref=v4.81.0"
   storage_mover_source_endpoints = local.storage_mover_source_endpoints
   depends_on                     = [module.storage_movers]
 }
 
 module "storage_mover_target_endpoints" {
-  source                         = "git::https://github.com/AeternaModules/azurerm_storage_mover_target_endpoint.git?ref=v4.80.0"
+  source                         = "git::https://github.com/AeternaModules/azurerm_storage_mover_target_endpoint.git?ref=v4.81.0"
   storage_mover_target_endpoints = local.storage_mover_target_endpoints
   depends_on                     = [module.storage_movers]
 }
 
 module "storage_mover_job_definitions" {
-  source                        = "git::https://github.com/AeternaModules/azurerm_storage_mover_job_definition.git?ref=v4.80.0"
+  source                        = "git::https://github.com/AeternaModules/azurerm_storage_mover_job_definition.git?ref=v4.81.0"
   storage_mover_job_definitions = local.storage_mover_job_definitions
   depends_on                    = [module.storage_mover_projects]
 }
